0d43dbb02a5ef15607dfd29aaeca254de8471259
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-11-26 16:52:58 +0100

    adminer export + add content + question 3 view artist

--- Spotiflix.sql
+++ Spotiflix.sql
@@ -1,164 +1,126 @@
--- -------------------------------------------------------------
--- TablePlus 5.1.0(468)
---
--- https://tableplus.com/
---
--- Database: Spotiflix
--- Generation Time: 2022-11-24 16:05:34.7250
--- -------------------------------------------------------------
+-- Adminer 4.8.1 PostgreSQL 14.5 dump
 
+\connect "Spotiflix";
 
-DROP TABLE IF EXISTS "public"."album";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
+DROP TABLE IF EXISTS "album";
+DROP SEQUENCE IF EXISTS album_id_seq;
+CREATE SEQUENCE album_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 3 CACHE 1;
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS album_id_seq;
-
--- Table Definition
 CREATE TABLE "public"."album" (
-    "id" int4 NOT NULL DEFAULT nextval('album_id_seq'::regclass),
-    "name" varchar(128) NOT NULL,
-    PRIMARY KEY ("id")
-);
+    "id" integer DEFAULT nextval('album_id_seq') NOT NULL,
+    "name" character varying(128) NOT NULL,
+    CONSTRAINT "album_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
 
-DROP TABLE IF EXISTS "public"."album_rent";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
--- Table Definition
+DROP TABLE IF EXISTS "album_rent";
 CREATE TABLE "public"."album_rent" (
-    "album_id" int4 NOT NULL,
-    "customer_id" int4 NOT NULL,
+    "album_id" integer NOT NULL,
+    "customer_id" integer NOT NULL,
     "borrow_date" date NOT NULL,
     "return_date" date,
-    PRIMARY KEY ("album_id","customer_id")
-);
+    CONSTRAINT "album_rent_album_id_customer_id" PRIMARY KEY ("album_id", "customer_id")
+) WITH (oids = false);
 
-DROP TABLE IF EXISTS "public"."album_song";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
--- Table Definition
+DROP TABLE IF EXISTS "album_song";
 CREATE TABLE "public"."album_song" (
-    "album_id" int4 NOT NULL,
-    "song_id" int4 NOT NULL,
-    "track" int4 NOT NULL CHECK (track > 0),
-    PRIMARY KEY ("album_id","song_id")
-);
+    "album_id" integer NOT NULL,
+    "song_id" integer NOT NULL,
+    "track" integer NOT NULL,
+    CONSTRAINT "album_song_album_id_song_id" PRIMARY KEY ("album_id", "song_id"),
+    CONSTRAINT "album_song_album_id_track" UNIQUE ("album_id", "track")
+) WITH (oids = false);
 
-DROP TABLE IF EXISTS "public"."album_stock";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS album_stock_id_seq;
+DROP TABLE IF EXISTS "album_stock";
+DROP SEQUENCE IF EXISTS album_stock_id_seq;
+CREATE SEQUENCE album_stock_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 5 CACHE 1;
 
--- Table Definition
 CREATE TABLE "public"."album_stock" (
-    "id" int4 NOT NULL DEFAULT nextval('album_stock_id_seq'::regclass),
-    "album_id" int4 NOT NULL,
-    "stock" int4 NOT NULL CHECK (stock >= 0),
-    PRIMARY KEY ("id")
-);
+    "id" integer DEFAULT nextval('album_stock_id_seq') NOT NULL,
+    "album_id" integer NOT NULL,
+    "stock" integer NOT NULL,
+    CONSTRAINT "album_stock_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
 
-DROP TABLE IF EXISTS "public"."artist";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS artist_id_seq;
+DROP TABLE IF EXISTS "artist";
+DROP SEQUENCE IF EXISTS artist_id_seq;
+CREATE SEQUENCE artist_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 9 CACHE 1;
 
--- Table Definition
 CREATE TABLE "public"."artist" (
-    "id" int4 NOT NULL DEFAULT nextval('artist_id_seq'::regclass),
-    "name" varchar(128) NOT NULL,
+    "id" integer DEFAULT nextval('artist_id_seq') NOT NULL,
+    "name" character varying(128) NOT NULL,
     "birthdate" date NOT NULL,
-    PRIMARY KEY ("id")
-);
+    CONSTRAINT "artist_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
 
-DROP TABLE IF EXISTS "public"."customer";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS customer_id_seq;
+DROP TABLE IF EXISTS "customer";
+DROP SEQUENCE IF EXISTS customer_id_seq;
+CREATE SEQUENCE customer_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 4 CACHE 1;
 
--- Table Definition
 CREATE TABLE "public"."customer" (
-    "id" int4 NOT NULL DEFAULT nextval('customer_id_seq'::regclass),
-    "email" varchar(128) NOT NULL,
-    "firstname" varchar(64) NOT NULL CHECK (initcap((firstname)::text) = (firstname)::text),
-    "lastname" varchar(64) NOT NULL CHECK (upper((lastname)::text) = (lastname)::text),
-    PRIMARY KEY ("id")
-);
+    "id" integer DEFAULT nextval('customer_id_seq') NOT NULL,
+    "email" character varying(128) NOT NULL,
+    "firstname" character varying(64) NOT NULL,
+    "lastname" character varying(64) NOT NULL,
+    CONSTRAINT "customer_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
 
-DROP TABLE IF EXISTS "public"."song";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS song_id_seq;
+DROP TABLE IF EXISTS "song";
+DROP SEQUENCE IF EXISTS song_id_seq;
+CREATE SEQUENCE song_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 27 CACHE 1;
 
--- Table Definition
 CREATE TABLE "public"."song" (
-    "id" int4 NOT NULL DEFAULT nextval('song_id_seq'::regclass),
-    "name" varchar(128) NOT NULL,
-    "duration" int4 NOT NULL CHECK (duration > 0),
-    "artist_id" int4 NOT NULL,
-    PRIMARY KEY ("id")
-);
+    "id" integer DEFAULT nextval('song_id_seq') NOT NULL,
+    "name" character varying(128) NOT NULL,
+    "duration" integer NOT NULL,
+    "artist_id" integer NOT NULL,
+    CONSTRAINT "song_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
 
--- Column Comment
 COMMENT ON COLUMN "public"."song"."duration" IS 'secondes';
 
-DROP TABLE IF EXISTS "public"."track_deliveries";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS track_deliveries_id_seq;
+DROP TABLE IF EXISTS "track_deliveries";
+DROP SEQUENCE IF EXISTS track_deliveries_id_seq;
+CREATE SEQUENCE track_deliveries_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 4 CACHE 1;
 
--- Table Definition
 CREATE TABLE "public"."track_deliveries" (
-    "id" int4 NOT NULL DEFAULT nextval('track_deliveries_id_seq'::regclass),
-    "album_stock_id" int4 NOT NULL,
-    "amount" int4 NOT NULL,
+    "id" integer DEFAULT nextval('track_deliveries_id_seq') NOT NULL,
+    "album_stock_id" integer NOT NULL,
+    "amount" integer NOT NULL,
     "delivery_date" date NOT NULL,
-    PRIMARY KEY ("id")
-);
+    CONSTRAINT "track_deliveries_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
 
-INSERT INTO "public"."album" ("id", "name") VALUES
-(1, 'Meddle');
 
-INSERT INTO "public"."album_rent" ("album_id", "customer_id", "borrow_date", "return_date") VALUES
-(1, 1, '2022-11-23', NULL);
+DROP VIEW IF EXISTS "view_artists";
+CREATE TABLE "view_artists" ("name" character varying(128), "birthdate" date, "songs" bigint);
 
-INSERT INTO "public"."album_song" ("album_id", "song_id", "track") VALUES
-(1, 1, 6),
-(1, 2, 5),
-(1, 3, 4),
-(1, 4, 3),
-(1, 5, 2),
-(1, 6, 1);
 
-INSERT INTO "public"."album_stock" ("id", "album_id", "stock") VALUES
-(1, 1, 10);
+ALTER TABLE ONLY "public"."album_rent" ADD CONSTRAINT "album_rent_album_id_fkey" FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."album_rent" ADD CONSTRAINT "album_rent_customer_id_fkey" FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE NOT DEFERRABLE;
 
-INSERT INTO "public"."artist" ("id", "name", "birthdate") VALUES
-(1, 'Pink Floyd', '1966-01-01');
+ALTER TABLE ONLY "public"."album_song" ADD CONSTRAINT "album_song_album_id_fkey" FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."album_song" ADD CONSTRAINT "album_song_song_id_fkey" FOREIGN KEY (song_id) REFERENCES song(id) ON DELETE CASCADE NOT DEFERRABLE;
 
-INSERT INTO "public"."customer" ("id", "email", "firstname", "lastname") VALUES
-(1, 'quentin.peltier.pro@gmail.com', 'Quentin', 'PELTIER'),
-(4, 'gatien.anizan@gmail.com', 'Gatien', 'ANIZAN');
+ALTER TABLE ONLY "public"."album_stock" ADD CONSTRAINT "album_stock_album_id_fkey" FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE CASCADE NOT DEFERRABLE;
 
-INSERT INTO "public"."song" ("id", "name", "duration", "artist_id") VALUES
-(1, 'Echoes', 1413, 1),
-(2, 'Seamus', 134, 1),
-(3, 'San Tropez', 222, 1),
-(4, 'Fearless', 367, 1),
-(5, 'A Pillow Of Winds', 313, 1),
-(6, 'On Of These Days', 355, 1);
+ALTER TABLE ONLY "public"."song" ADD CONSTRAINT "song_artist_id_fkey" FOREIGN KEY (artist_id) REFERENCES artist(id) ON DELETE CASCADE NOT DEFERRABLE;
 
-INSERT INTO "public"."track_deliveries" ("id", "album_stock_id", "amount", "delivery_date") VALUES
-(1, 1, 1, '2022-11-24');
+ALTER TABLE ONLY "public"."track_deliveries" ADD CONSTRAINT "track_deliveries_album_stock_id_fkey" FOREIGN KEY (album_stock_id) REFERENCES album_stock(id) ON DELETE CASCADE NOT DEFERRABLE;
 
-ALTER TABLE "public"."album_rent" ADD FOREIGN KEY ("customer_id") REFERENCES "public"."customer"("id") ON DELETE CASCADE;
-ALTER TABLE "public"."album_rent" ADD FOREIGN KEY ("album_id") REFERENCES "public"."album"("id") ON DELETE CASCADE;
-ALTER TABLE "public"."album_song" ADD FOREIGN KEY ("album_id") REFERENCES "public"."album"("id") ON DELETE CASCADE;
-ALTER TABLE "public"."album_song" ADD FOREIGN KEY ("song_id") REFERENCES "public"."song"("id") ON DELETE CASCADE;
-ALTER TABLE "public"."album_stock" ADD FOREIGN KEY ("album_id") REFERENCES "public"."album"("id") ON DELETE CASCADE;
-ALTER TABLE "public"."song" ADD FOREIGN KEY ("artist_id") REFERENCES "public"."artist"("id") ON DELETE CASCADE;
-ALTER TABLE "public"."track_deliveries" ADD FOREIGN KEY ("album_stock_id") REFERENCES "public"."album_stock"("id") ON DELETE CASCADE;
+DROP TABLE IF EXISTS "view_artists";
+CREATE VIEW "view_artists" AS SELECT artist.name,
+    artist.birthdate,
+    count(song.id) AS songs
+   FROM (artist
+     JOIN song ON ((song.artist_id = artist.id)))
+  GROUP BY artist.name, artist.birthdate
+  ORDER BY artist.name;
+
+-- 2022-11-26 15:49:59.806247+00

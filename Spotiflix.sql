c389497e8adcfa8ee1438c5a9bb754e1b222ecfc
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-11-28 16:11:42 +0100

    add data + question 1

--- Spotiflix.sql
+++ Spotiflix.sql
@@ -1,7 +1,3 @@
--- Adminer 4.8.1 PostgreSQL 14.5 dump
-
-\connect "Spotiflix";
-
 DROP VIEW IF EXISTS "VIEW_ALBUMS";
 CREATE TABLE "VIEW_ALBUMS" ("album" character varying(128), "songs" bigint, "duration" bigint);
 
@@ -20,6 +16,10 @@ CREATE TABLE "public"."album" (
     CONSTRAINT "album_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
+INSERT INTO "album" ("id", "name") VALUES
+(1,	'Meddle'),
+(2,	'Modal Soul'),
+(3,	'Mirage');
 
 DROP TABLE IF EXISTS "album_rent";
 CREATE TABLE "public"."album_rent" (
@@ -30,6 +30,10 @@ CREATE TABLE "public"."album_rent" (
     CONSTRAINT "album_rent_album_id_customer_id" PRIMARY KEY ("album_id", "customer_id")
 ) WITH (oids = false);
 
+INSERT INTO "album_rent" ("album_id", "customer_id", "borrow_date", "return_date") VALUES
+(1,	1,	'2022-11-23',	NULL),
+(2,	1,	'2022-10-03',	'2022-10-22'),
+(3,	2,	'2022-11-26',	NULL);
 
 DROP TABLE IF EXISTS "album_song";
 CREATE TABLE "public"."album_song" (
@@ -40,6 +44,32 @@ CREATE TABLE "public"."album_song" (
     CONSTRAINT "album_song_album_id_track" UNIQUE ("album_id", "track")
 ) WITH (oids = false);
 
+INSERT INTO "album_song" ("album_id", "song_id", "track") VALUES
+(1,	6,	1),
+(1,	5,	2),
+(1,	4,	3),
+(1,	3,	4),
+(1,	2,	5),
+(1,	1,	6),
+(2,	9,	1),
+(2,	10,	2),
+(2,	11,	3),
+(2,	12,	4),
+(2,	13,	5),
+(2,	14,	6),
+(2,	15,	7),
+(2,	16,	8),
+(2,	17,	9),
+(2,	18,	10),
+(2,	19,	11),
+(2,	20,	12),
+(2,	21,	13),
+(2,	22,	14),
+(3,	23,	1),
+(3,	24,	2),
+(3,	25,	3),
+(3,	26,	4),
+(3,	27,	5);
 
 DROP TABLE IF EXISTS "album_stock";
 DROP SEQUENCE IF EXISTS album_stock_id_seq;
@@ -52,10 +82,14 @@ CREATE TABLE "public"."album_stock" (
     CONSTRAINT "album_stock_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
+INSERT INTO "album_stock" ("id", "album_id", "stock") VALUES
+(1,	1,	10),
+(2,	2,	4),
+(3,	3,	16);
 
 DROP TABLE IF EXISTS "artist";
 DROP SEQUENCE IF EXISTS artist_id_seq;
-CREATE SEQUENCE artist_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 9 CACHE 1;
+CREATE SEQUENCE artist_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 11 CACHE 1;
 
 CREATE TABLE "public"."artist" (
     "id" integer DEFAULT nextval('artist_id_seq') NOT NULL,
@@ -64,6 +98,16 @@ CREATE TABLE "public"."artist" (
     CONSTRAINT "artist_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
+INSERT INTO "artist" ("id", "name", "birthdate") VALUES
+(1,	'Pink Floyd',	'1966-01-01'),
+(2,	'Nujabes',	'1974-02-26'),
+(3,	'Camel',	'1970-01-01'),
+(4,	'Daft Punk',	'1993-01-01'),
+(5,	'Mézigue',	'2016-01-01'),
+(6,	'DJ Premier',	'1966-03-21'),
+(7,	'Tangerine Dream',	'1967-01-01'),
+(8,	'Chromatics',	'2001-01-01'),
+(9,	'Magic Sword',	'2013-01-01');
 
 DROP TABLE IF EXISTS "customer";
 DROP SEQUENCE IF EXISTS customer_id_seq;
@@ -77,6 +121,9 @@ CREATE TABLE "public"."customer" (
     CONSTRAINT "customer_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
+INSERT INTO "customer" ("id", "email", "firstname", "lastname") VALUES
+(1,	'quentin.peltier.pro@gmail.com',	'Quentin',	'PELTIER'),
+(2,	'gatien.anizan@gmail.com',	'Gatien',	'ANIZAN');
 
 DROP TABLE IF EXISTS "song";
 DROP SEQUENCE IF EXISTS song_id_seq;
@@ -92,6 +139,32 @@ CREATE TABLE "public"."song" (
 
 COMMENT ON COLUMN "public"."song"."duration" IS 'secondes';
 
+INSERT INTO "song" ("id", "name", "duration", "artist_id") VALUES
+(1,	'Echoes',	1413,	1),
+(2,	'Seamus',	134,	1),
+(3,	'San Tropez',	222,	1),
+(4,	'Fearless',	367,	1),
+(5,	'A Pillow Of Winds',	313,	1),
+(6,	'On Of These Days',	355,	1),
+(9,	'Feather (feat. Cise Starr & Akin from CYNE)',	175,	2),
+(10,	'ordinary joe (feat. Terry Callier)',	308,	2),
+(11,	'reflection eternal',	257,	2),
+(12,	'Luv(sic.) pt3 (feat. Shing02)',	336,	2),
+(13,	'Music is mine',	260,	2),
+(14,	'Eclipse (feat. Substantial)',	394,	2),
+(15,	'The Sign (feat. Pase Rock)',	289,	2),
+(16,	'Thank you (feat. Apani B)',	249,	2),
+(17,	'World''s end Rhapsody',	341,	2),
+(18,	'Modal Soul (feat. Uyama Hiroto)',	281,	2),
+(19,	'flowers',	239,	2),
+(20,	'sea of cloud',	181,	2),
+(21,	'Light on the land',	235,	2),
+(22,	'Horizon',	440,	2),
+(23,	'Freefall',	354,	3),
+(24,	'Supertwister',	203,	3),
+(25,	'Nimrodel / The Procession / The White Rider',	558,	3),
+(26,	'Earthrise',	401,	3),
+(27,	'Lady Fantasy: Encounter / Smiles for You / Lady Fantasy',	765,	3);
 
 DROP TABLE IF EXISTS "track_deliveries";
 DROP SEQUENCE IF EXISTS track_deliveries_id_seq;
@@ -105,6 +178,10 @@ CREATE TABLE "public"."track_deliveries" (
     CONSTRAINT "track_deliveries_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
+INSERT INTO "track_deliveries" ("id", "album_stock_id", "amount", "delivery_date") VALUES
+(1,	1,	1,	'2022-11-24'),
+(2,	2,	2,	'2022-10-04'),
+(3,	3,	4,	'2022-11-27');
 
 ALTER TABLE ONLY "public"."album_rent" ADD CONSTRAINT "album_rent_album_id_fkey" FOREIGN KEY (album_id) REFERENCES album(id) ON DELETE CASCADE NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."album_rent" ADD CONSTRAINT "album_rent_customer_id_fkey" FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE NOT DEFERRABLE;
@@ -137,4 +214,19 @@ CREATE VIEW "VIEW_ARTISTS" AS SELECT artist.name AS artist,
   GROUP BY artist.name, artist.birthdate
   ORDER BY artist.name;
 
--- 2022-11-26 16:10:37.029421+00
+
+CREATE OR REPLACE FUNCTION ADD_ARTIST(a_name VARCHAR(128), a_birthdate DATE)
+  RETURNS boolean
+  AS $$
+DECLARE
+    artist_id integer;
+BEGIN
+    SELECT id INTO artist_id FROM artist WHERE artist.name = a_name;
+    IF artist_id IS NULL THEN
+        INSERT INTO artist (name, birthdate) VALUES (a_name, a_birthdate);
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
